dc629b09312e91cbfd6b2f8c8fceda4ca810f58e
squhart <squhart@gmail.com>
2016-04-25 12:14:30 +0700

    new cop view

--- application/controllers/Cop.php
+++ application/controllers/Cop.php
@@ -5,6 +5,15 @@
 */
 class Cop extends CI_Controller
 {
-	
+	public function index()
+	{
+		$data['title'] = "COP";
+		$this->load->view('cop');
+	}
+
+	public function FunctionName($value='')
+	{
+		# code...
+	}
 
 }
---
+++ application/views/cop.php
@@ -0,0 +1,106 @@
+<?php include "header.php" ?>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 isi">
+				<div class="col-md-4 pull-right">
+              <!-- Widget: user widget style 1 -->
+              <div class="box box-widget widget-user-2">
+                <!-- Add the bg color to the header using any of the bg-* classes -->
+                <div class="box-header bg-blue">
+                  <h3>
+                  	<a class="clearlink" href="">
+                  		Teuing ah
+                  	</a>
+                  </h3>
+                  <h5 class="widget-user-desc">Nama si expert</h5>
+                </div>
+                <div class="box-footer no-padding">
+                  <ul class="nav nav-stacked">
+                    <li><a href="#">Journal <span class="pull-right badge bg-blue">31</span></a></li>
+                    <li><a href="#">Course <span class="pull-right badge bg-aqua">5</span></a></li>
+                    <li><a href="#">Discussion <span class="pull-right badge bg-green">12</span></a></li>
+                  </ul>
+                </div>
+              </div><!-- /.widget-user -->
+            </div>
+            	<div class="col-md-8">
+						<a href="" class="btn btn-flat btn-lg btn-primary pull-right">New Innovation</a>
+						<a href="" title="Archive" class="btn btn-flat btn-lg bg-navy pull-right"><span class="fa fa-file-archive-o"></span></a>
+						<h3>Innovation</h3>					
+							<div class="box box-primary">
+								<div class="box-header with-border">
+									<h3 class="box-title"><strong><a href="">Kenapa bulan itu bulat, gak kotak atau segitiga?</a></strong></h3>
+										<div class="author">
+												Oleh : 
+											<a href="">
+												<strong>Ucu</strong>
+											</a>
+										</div>
+								</div>
+								<div class="box-body">
+									<p>cutted description</p>
+									<span class="pull-right badge bg-blue">direktorat</span>
+								</div>
+							</div>
+					
+						<div class="box box-primary">
+							<div class="box-header with-border">
+								<h3 class="box-title"><strong><a href="">Kenapa bulan itu bulat, gak kotak atau segitiga?</a></strong></h3>
+									<div class="author">
+											Oleh : 
+										<a href="">
+											<strong>Ucu</strong>
+										</a>
+									</div>
+							</div>
+							<div class="box-body">
+								<p>cutted description</p>
+								<span class="pull-right badge bg-blue">direktorat</span>
+							</div>
+						</div>
+            	<hr>
+            	</div>
+
+            	<div class="col-md-8">
+						<a href="" class="btn btn-flat btn-lg btn-primary pull-right">New Best Practice</a>
+						<a href="" title="Archive" class="btn btn-flat btn-lg bg-navy pull-right"><span class="fa fa-file-archive-o"></span></a>
+						<h3>Best Practice</h3>					
+							<div class="box box-primary">
+								<div class="box-header with-border">
+									<h3 class="box-title"><strong><a href="">Kenapa bulan itu bulat, gak kotak atau segitiga?</a></strong></h3>
+										<div class="author">
+												Oleh : 
+											<a href="">
+												<strong>Ucu</strong>
+											</a>
+										</div>
+								</div>
+								<div class="box-body">
+									<p>cutted description</p>
+									<span class="pull-right badge bg-blue">direktorat</span>
+								</div>
+							</div>
+					
+						<div class="box box-primary">
+							<div class="box-header with-border">
+								<h3 class="box-title"><strong><a href="">Kenapa bulan itu bulat, gak kotak atau segitiga?</a></strong></h3>
+									<div class="author">
+											Oleh : 
+										<a href="">
+											<strong>Ucu</strong>
+										</a>
+									</div>
+							</div>
+							<div class="box-body">
+								<p>cutted description</p>
+								<span class="pull-right badge bg-blue">direktorat</span>
+							</div>
+						</div>
+            	</div>
+
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
